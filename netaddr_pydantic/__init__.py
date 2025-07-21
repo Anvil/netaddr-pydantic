bf31dbd7eba465ac9ebd9a8bfc4da295587771a5
Damien Nadé <anvil.github@livna.org>
2025-07-21 19:37:29 +0200

    add __version__

--- netaddr_pydantic/__init__.py
+++ netaddr_pydantic/__init__.py
@@ -13,6 +13,9 @@ from pydantic import PlainSerializer, PlainValidator, Tag, Discriminator
 from pydantic_core import PydanticCustomError
 
 
+__version__ = "0.0.1"
+
+
 IPAddress = Annotated[
     netaddr.IPAddress, PlainValidator(netaddr.IPAddress), PlainSerializer(str)
 ]
