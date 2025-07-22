dc8864c65a2f23eba779a8de680d044a03e31cd5
Damien Nadé <anvil.github@livna.org>
2025-07-23 01:38:48 +0200

    add test test_ipv46xxx

--- tests/unit/test_ipany.py
+++ tests/unit/test_ipany.py
@@ -1,14 +1,22 @@
-"""Unit tests for netaddr_pydantic.IPAny"""
+"""Unit tests for netaddr_pydantic.IPAny and other IPvXAddress/Network"""
 
+from contextlib import nullcontext as does_not_raise, AbstractContextManager
 from typing import TypeAlias, Any
 
 import netaddr
 import pytest
 from pydantic_core import PydanticCustomError
+from pydantic import ValidationError
 
-from netaddr_pydantic import IPAny
+from netaddr_pydantic import (
+    IPAny,
+    IPv4Address,
+    IPv6Address,
+    IPv4Network,
+    IPv6Network,
+)
 
-from .conftest import GenericModel
+from .conftest import GenericModel, InetFixture
 
 
 @pytest.mark.parametrize(
@@ -35,3 +43,38 @@ def test_bad_ipany() -> None:
     Model: TypeAlias = GenericModel[IPAny]
     with pytest.raises(PydanticCustomError):
         Model(field=1)
+
+
+@pytest.mark.parametrize("address", ("1.2.3.0", "dead:beef::1"))
+@pytest.mark.parametrize("netaddr_t", (netaddr.IPNetwork, netaddr.IPAddress))
+def test_ipv46xxx(
+    inet: InetFixture,
+    address: str,
+    netaddr_t: type[netaddr.IPNetwork | netaddr.IPAddress],
+) -> None:
+    """Validate that IPv4Address/IPv4Network fields validate actual IPv4
+    addresses and refuses IPv6 addresses. And vice versa.
+    And vice-versa....
+    """
+    model_type: type[GenericModel[Any]]
+    match (inet, netaddr_t):
+        case (4, netaddr.IPNetwork):
+            model_type = GenericModel[IPv4Network]
+        case (6, netaddr.IPNetwork):
+            model_type = GenericModel[IPv6Network]
+        case (4, netaddr.IPAddress):
+            model_type = GenericModel[IPv4Address]
+        case (6, netaddr.IPAddress):
+            model_type = GenericModel[IPv6Address]
+
+    if netaddr_t == netaddr.IPNetwork:
+        address += "/24"
+
+    expectation: AbstractContextManager[Any]
+    if inet == netaddr_t(address).version:
+        expectation = does_not_raise()
+    else:
+        expectation = pytest.raises(ValidationError)
+
+    with expectation:
+        model_type(field=address)
