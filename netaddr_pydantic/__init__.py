1bb335a282dd696df5c13a89fbca32f6dfca4e32
Damien Nadé <anvil.github@livna.org>
2025-07-23 01:38:22 +0200

    move functions to their dedicated file and implement IPv[46](Address|Network) types

--- netaddr_pydantic/__init__.py
+++ netaddr_pydantic/__init__.py
@@ -3,15 +3,18 @@ annotations in pydantic models.
 
 """
 
-from typing import Any, Annotated
-
+from typing import Annotated
 
 import netaddr
-from netaddr.core import AddrFormatError
-
 from pydantic import PlainSerializer, PlainValidator, Tag, Discriminator
-from pydantic_core import PydanticCustomError
 
+from .friends import (
+    ipv,
+    iprange_validator,
+    ipset_validator,
+    ipset_serializer,
+    ipany_discriminator,
+)
 
 __version__ = "0.1.2"
 
@@ -20,43 +23,34 @@ IPAddress = Annotated[
     netaddr.IPAddress, PlainValidator(netaddr.IPAddress), PlainSerializer(str)
 ]
 
+IPv4Address = Annotated[
+    netaddr.IPAddress, ipv(4, netaddr.IPAddress), PlainSerializer(str)
+]
+
+IPv6Address = Annotated[
+    netaddr.IPAddress, ipv(6, netaddr.IPAddress), PlainSerializer(str)
+]
+
 IPNetwork = Annotated[
     netaddr.IPNetwork, PlainValidator(netaddr.IPNetwork), PlainSerializer(str)
 ]
+
+IPv4Network = Annotated[
+    netaddr.IPNetwork, ipv(4, netaddr.IPNetwork), PlainSerializer(str)
+]
+
+IPv6Network = Annotated[
+    netaddr.IPNetwork, ipv(6, netaddr.IPNetwork), PlainSerializer(str)
+]
+
 
 IPGlob = Annotated[
     netaddr.IPGlob, PlainValidator(netaddr.IPGlob), PlainSerializer(str)
 ]
 
-
-def iprange_validator(value: Any) -> netaddr.IPRange:
-    """Allow strings and some kinds of tuple/list to be used as IPRange"""
-    if isinstance(value, (tuple, list)) and len(value) == 2:
-        return netaddr.IPRange(*value)
-    if isinstance(value, str):
-        try:
-            start, end = value.split("-", maxsplit=1)
-            return netaddr.IPRange(start, end)
-        except (TypeError, ValueError, AddrFormatError):
-            pass
-    raise ValueError(value)
-
-
 IPRange = Annotated[
     netaddr.IPRange, PlainValidator(iprange_validator), PlainSerializer(str)
 ]
-
-
-def ipset_validator(value: Any) -> netaddr.IPSet:
-    """Data validation for IPSet"""
-    if isinstance(value, (tuple, list, set)):
-        return netaddr.IPSet(value)
-    raise ValueError(value)
-
-
-def ipset_serializer(ipset: netaddr.IPSet) -> list[str]:
-    """IPSet Serialization as list of strings"""
-    return [str(cidr) for cidr in ipset.iter_cidrs()]
 
 
 IPSet = Annotated[
@@ -64,25 +58,6 @@ IPSet = Annotated[
     PlainValidator(ipset_validator, json_schema_input_type=list[str | int]),
     PlainSerializer(ipset_serializer, return_type=list[str]),
 ]
-
-
-def ipany_discriminator(value: Any) -> str:
-    """Attempt to detect most adapted class from input value"""
-    if isinstance(value, (list, tuple, set)):
-        return "set"
-    if not isinstance(value, str):
-        raise PydanticCustomError(
-            "netaddr_pydantic_error",
-            "unexpected type {value_type} for netaddr conversion",
-            {"value_type": type(value), "value": value},
-        )
-    if "/" in value:
-        return "network"
-    if "-" in value:
-        return "range"
-    if "*" in value:
-        return "glob"
-    return "address"
 
 
 IPAny = Annotated[
@@ -94,4 +69,15 @@ IPAny = Annotated[
     Discriminator(ipany_discriminator),
 ]
 
-__all__ = ("IPAddress", "IPNetwork", "IPGlob", "IPRange", "IPSet", "IPAny")
+__all__ = (
+    "IPAddress",
+    "IPv4Address",
+    "IPv6Address",
+    "IPNetwork",
+    "IPv4Network",
+    "IPv6Network",
+    "IPGlob",
+    "IPRange",
+    "IPSet",
+    "IPAny",
+)
